2e97b765ad05212f6e5ad3a03fd6fcbb8478ceb4
LazyYeou <lazyyeou@gmail.com>
2026-02-04 07:56:38 +0700

    feat: init db

--- src/2/gcs-web-app/backend/main.go
+++ src/2/gcs-web-app/backend/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"database/sql"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -11,3 +14,29 @@ type Waypoint struct {
 	Latitude  float64 `json:"latitude" binding:"required,min=-90,max=190"`
 	Altitude  float64 `json:"altitude" binding:"required,min=0"`
 }
+
+var db *sql.DB
+
+dbPath := os.GetEnv("DB_PATH")
+if dbPath == "" {
+	dbPath = "/data/waypoints.db"
+}
+
+db, err = sql.Open("sqlite3", dbPath)
+if err != nil {
+	log.Fatal(err)
+}
+
+createTableSQL := `CREATE TABLE IF NOT EXISTS waypoints (
+	id TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	longitude REAL NOT NULL,
+	latitude REAL NOT NULL,
+	altitude REAL NOT NULL
+);`
+
+x, err = db.Exec(createTableSQL)
+if err != nil {
+	log.Fatal(err)
+}
+
