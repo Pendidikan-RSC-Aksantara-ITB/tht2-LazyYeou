77c187cb040f22c237f6840879dfed2957a96a05
LazyYeou <lazyyeou@gmail.com>
2026-02-04 09:05:52 +0700

    feat: add waypoint query function

--- src/2/gcs-web-app/backend/main.go
+++ src/2/gcs-web-app/backend/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"os"
 	"database/sql"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -17,26 +20,63 @@ type Waypoint struct {
 
 var db *sql.DB
 
-dbPath := os.GetEnv("DB_PATH")
-if dbPath == "" {
-	dbPath = "/data/waypoints.db"
-}
+func initDB() {
+	dbPath := os.Getenv("DB_PATH")
+	if dbPath == "" {
+		dbPath = "/data/waypoints.db"
+	}
 
-db, err = sql.Open("sqlite3", dbPath)
-if err != nil {
-	log.Fatal(err)
-}
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-createTableSQL := `CREATE TABLE IF NOT EXISTS waypoints (
+	createTableSQL := `CREATE TABLE IF NOT EXISTS waypoints (
 	id TEXT PRIMARY KEY,
 	name TEXT NOT NULL,
 	longitude REAL NOT NULL,
 	latitude REAL NOT NULL,
 	altitude REAL NOT NULL
-);`
+	);`
 
-x, err = db.Exec(createTableSQL)
-if err != nil {
-	log.Fatal(err)
+	_, err = db.Exec(createTableSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
+func getWaypoints(c *gin.Context) {
+	rows, err := db.Query("SELECT id, name, longitude, latitude, altitude FROM waypoints")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	waypoints := []Waypoint{}
+	for rows.Next() {
+		var w Waypoint
+		err := rows.Scan(&w.ID, &w.Name, &w.Longitude, &w.Latitude, &w.Altitude)
+		if err != nil {
+			return
+		}
+		waypoints = append(waypoints, w)
+	}
+	c.JSON(http.StatusOK, waypoints)
+}
+
+func saveWaypoints(c *gin.Context) {
+
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	port := "8081"
+	r.Run(":" + port)
+}
